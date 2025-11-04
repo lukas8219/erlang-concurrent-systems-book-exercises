6b3957de1f9eb7e19daf374baf5850c81143729e
lukas8219 <lucas.c4d@gmail.com>
2025-11-03 22:12:24 -0300

    [dqueue] working with mnesia

--- chapter-11/dqueue.erl
+++ chapter-11/dqueue.erl
@@ -1,7 +1,7 @@
 -module(dqueue).
 
 -export([handle_call/3, handle_cast/2, init/1, start_link/1, publish/1, acknowledge/1,
-         handle_info/2, consumer_loop/0, start_consumer/0, get/0]).
+         get/0]).
 
 -behavior(gen_server).
 
@@ -15,61 +15,32 @@ start_link(Name) ->
 %% how to handle name
 init(Name) ->
     {ok, DBPid} = dqueue_db:start_database(Name),
+    dqueue_msg_store:start_database(Name),
     {ok, #queue_state{name = Name, db = DBPid}}.
 
 handle_call(_, _, State) ->
     {reply, ok, State}.
 
-handle_cast({publish, {_H, Message}}, State = #queue_state{name = Name}) ->
-    case ets:insert_new(EtsTable, {{Name, erlang:system_time()}, {_H, Message}}) of
-        true ->
+handle_cast({publish, Msg = {_Ts, _H, _Message}}, State) ->
+    PossiblySent = dqueue_db:send_if_any_idle(Msg),
+    case PossiblySent of
+        ok ->
             {noreply, State};
-        false ->
-            {noreply,
-             State} %% how to handle errors?. Might not be possible with handle_cast (for now)
+        dropped ->
+            dqueue_msg_store:write(Msg),
+            {noreply, State}
     end;
-handle_cast({publish, {_H, Message}},
-            State = #queue_state{idle_consumers = [NextConsumer | _Rest], in_flight = InFlight}) ->
-    Msg = {erlang:system_time(), {_H, Message}},
-    NextConsumer ! Msg,
-    {noreply, State#queue_state{idle_consumers = _Rest, in_flight = [Msg | InFlight]}};
-handle_cast({get, ConsumerRef},
-            State =
-                #queue_state{name = Name,
-                             ets_table = EtsTable,
-                             in_flight = InFlight,
-                             idle_consumers = IdleConsumers}) ->
-    %% start consumer
-    NewState =
-        case ets:match_object(EtsTable, {{Name, '_'}, '_'}, 1) of
-            {[{{_, MTs}, MMessage}], _} ->
-                ets:delete_object(EtsTable, {{Name, MTs}, MMessage}),
-                Msg1 = {MTs, MMessage},
-                ConsumerRef ! Msg1,
-                State#queue_state{in_flight = [Msg1 | InFlight]};
-            '$end_of_table' ->
-                erlang:monitor(process, ConsumerRef),
-                State#queue_state{idle_consumers = [ConsumerRef | IdleConsumers]}
-        end,
-    {noreply, NewState};
-handle_cast({acknowledge, {Ts, _}}, State = #queue_state{in_flight = InFlight}) ->
-    NewInFlight =
-        [{InflightMTs, InflightContent}
-         || {InflightMTs, InflightContent} <- InFlight, InflightMTs =/= Ts],
-    {noreply, State#queue_state{in_flight = NewInFlight}}.
-
-handle_info({'DOWN', _, process, _Pid, _Reason},
-            State = #queue_state{idle_consumers = []}) ->
-    {noreply, State};
-handle_info({'DOWN', _, process, Pid, Reason},
-            State =
-                #queue_state{idle_consumers = IdleConsumers,
-                             in_flight =
-                                 _InFlight}) -> %% ideally cleanse the messages in_flight for that consumer
-    io:format("Queue server (~p) has terminated with reason ~p~n", [Pid, Reason]),
-    % Perform cleanup or restart logic here.
-    NewIdleConsumers = [CPid || CPid <- IdleConsumers, CPid =/= Pid],
-    {noreply, State#queue_state{idle_consumers = NewIdleConsumers}}.
+handle_cast({get, ConsumerRef}, State) ->
+    PossiblyNextMessage = dqueue_msg_store:find_next_message(),
+    case PossiblyNextMessage of
+        {ok, '$end_of_table'} ->
+            dqueue_db:add_idle_consumer(ConsumerRef),
+            {noreply, State};
+        {ok, Message} ->
+            dqueue_db:add_inflight_message(Message),
+            ConsumerRef ! Message,
+            {noreply, State}
+    end.
 
 %% Client API
 get() ->
@@ -84,30 +55,5 @@ acknowledge({Ts, _Content}) ->
     gen_server:cast(?MODULE, {acknowledge, {Ts, _Content}}).
 
 publish({Header, Message}) ->
-    gen_server:cast(?MODULE, {publish, {Header, Message}}),
+    gen_server:cast(?MODULE, {publish, {erlang:system_time(), Header, Message}}),
     ok.
-
-%% Consumer API
-
-start_consumer() ->
-    Pid = spawn(cqueue, consumer_loop, []),
-    {ok, Pid}.
-
-consumer_loop() ->
-    Response = cqueue:get(),
-    case Response of
-        ok ->
-            io:format("[CONSUMER] Waiting for new messages...\n"),
-            consumer_loop0();
-        Failed ->
-            io:format("[CONSUMER] Failing process ~p\n", [Failed]),
-            exit(Failed)
-    end.
-
-consumer_loop0() ->
-    receive
-        Message ->
-            io:format("[CONSUMER] Recived ~p\ Sending ACK\n", [Message]),
-            cqueue:acknowledge(Message),
-            consumer_loop()
-    end.
--- chapter-11/dqueue_db.erl
+++ chapter-11/dqueue_db.erl
@@ -1,7 +1,7 @@
 -module(dqueue_db).
 
 -export([add_idle_consumer/1, remove_idle_consumer/1, add_inflight_message/1,
-         remove_inflight_message/1]).
+         remove_inflight_message/1, send_if_any_idle/1]).
 -export([handle_call/3, handle_cast/2, init/1, start_database/1]).
 
 -behavior(gen_server).
@@ -13,69 +13,60 @@ start_database(QueueName) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, QueueName, []).
 
 %% how to handle name
+%% this state is not distributed yet.
 init(QueueName) ->
-    {ok, TableName} = create_table_if_not_exists(QueueName),
     {ok,
-     #dqueue_db_state{table_name = TableName,
+     #dqueue_db_state{table_name = "mocked",
                       db_store_state =
                           #db_state{identifier = QueueName,
                                     in_flight = [],
                                     idle_consumers = []}}}.
 
-create_table_if_not_exists(Name) ->
-    case mnesia:create_table(Name,
-                             [{ram_only_copies, nodes()},
-                              {attributes, [record_info(fields, db_state)]}])
-    of
-        {atomic, ok} ->
-            io:format("Table ~p created successfully.~n", [Name]),
-            {ok, Name};
-        {aborted, {already_exists, _}} ->
-            io:format("Table ~p already exists.~n", [Name]);
-        {aborted, Reason} ->
-            io:format("Error creating table ~p: ~p~n", [Name, Reason])
-    end.
-
-handle_call({in_flight, add, {Ts, Content}},
+handle_call({in_flight, add, {Ts, _H, Content}},
             _Ref,
-            State =
-                #dqueue_db_state{table_name = TableName,
-                                 db_store_state = #db_state{in_flight = InFlight}}) ->
+            State = #dqueue_db_state{db_store_state = #db_state{in_flight = InFlight}}) ->
     NewInflight = [{Ts, Content} | InFlight],
     NewState = State#dqueue_db_state{db_store_state = #db_state{in_flight = NewInflight}},
-    mnesia:write(TableName, db_store_state),
     {reply, ok, NewState};
-handle_call({in_flight, remove, {Ts, _}},
+handle_call({in_flight, remove, {Ts, _H, _C}},
             _Ref,
-            State =
-                #dqueue_db_state{table_name = TableName,
-                                 db_store_state = #db_state{in_flight = InFlight}}) ->
+            State = #dqueue_db_state{db_store_state = #db_state{in_flight = InFlight}}) ->
     NewInFlight =
         [{InflightMTs, InflightContent}
          || {InflightMTs, InflightContent} <- InFlight, InflightMTs =/= Ts],
     NewState = State#dqueue_db_state{db_store_state = #db_state{in_flight = NewInFlight}},
-    mnesia:write(TableName, db_store_state),
     {reply, ok, NewState};
-handle_call({idle_consumers, add, {CPid}},
+handle_call({idle_consumers, add, CPid},
             _Ref,
-            State =
-                #dqueue_db_state{table_name = TableName,
-                                 db_store_state = #db_state{idle_consumers = IdleConsumers}}) ->
+            State = #dqueue_db_state{db_store_state = #db_state{idle_consumers = IdleConsumers}}) ->
     NewConsumers = [CPid | IdleConsumers],
     NewState =
         State#dqueue_db_state{db_store_state = #db_state{idle_consumers = NewConsumers}},
-    mnesia:write(TableName, db_store_state),
     {reply, ok, NewState};
-handle_call({idle_consumers, remove, {DCPid}},
+handle_call({idle_consumers, remove, DCPid},
             _Ref,
-            State =
-                #dqueue_db_state{table_name = TableName,
-                                 db_store_state = #db_state{idle_consumers = IdleConsumers}}) ->
+            State = #dqueue_db_state{db_store_state = #db_state{idle_consumers = IdleConsumers}}) ->
     NewIdleConsumers = [CPid || CPid <- IdleConsumers, CPid =/= DCPid],
     NewState =
         State#dqueue_db_state{db_store_state = #db_state{idle_consumers = NewIdleConsumers}},
-    mnesia:write(TableName, db_store_state),
-    {reply, ok, NewState}.
+    {reply, ok, NewState};
+handle_call({send_idle, Message},
+            _Ref,
+            State =
+                #dqueue_db_state{db_store_state =
+                                     #db_state{idle_consumers = [NextIdle | RestIdle],
+                                               in_flight = InFlight}}) ->
+    NewInflight = [Message | InFlight],
+    NewState =
+        State#dqueue_db_state{db_store_state =
+                                  #db_state{idle_consumers = RestIdle, in_flight = NewInflight}},
+    NextIdle ! Message,
+    {reply, ok, NewState};
+handle_call({send_idle, _Message},
+            _Ref,
+            State = #dqueue_db_state{db_store_state = #db_state{idle_consumers = []}}) ->
+    %% might need to remove from InFlight
+    {reply, dropped, State}.
 
 handle_cast(_, _) ->
     ok.
@@ -84,13 +75,16 @@ handle_cast(_, _) ->
 %%
 
 add_idle_consumer(Pid) ->
-    gen_server:call({idle_consumers, add}, Pid).
+    gen_server:call(?MODULE, {idle_consumers, add, Pid}).
 
 remove_idle_consumer(Pid) ->
-    gen_server:call({idle_consumers, remove}, Pid).
+    gen_server:call(?MODULE, {idle_consumers, remove, Pid}).
 
 add_inflight_message(Message) ->
-    gen_server:call({in_flight, add}, Message).
+    gen_server:call(?MODULE, {in_flight, add, Message}).
 
 remove_inflight_message(Message) ->
-    gen_server:call({in_flight, remove}, Message).
+    gen_server:call(?MODULE, {in_flight, remove, Message}).
+
+send_if_any_idle(Message) ->
+    gen_server:call(?MODULE, {send_idle, Message}).
